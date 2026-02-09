d713bc9085e0768d7a41552e74c1cd84e21bd16d
em828kentacuk <em828@kent.ac.uk>
2026-02-09 13:50:12 +0000

    completed 4

--- part_4.py
+++ part_4.py
@@ -1,1 +1,34 @@
 
+inventory = []
+
+status = [
+    {"Power": "100"},
+    {"Samples": "0"},
+
+]
+
+
+start_mission = input("Start mission? (y/n): ")
+
+
+travel_log = []
+
+if start_mission == "y":
+    while True:
+        try:
+            angle = int(input("Enter angle (0-90): "))
+        except ValueError:
+            print("Error: Corrupted Signal. Unable to retrieve angle.")
+            continue
+        if angle > 45:
+            print("CRITICAL TILT! HALTING.")
+        else:
+            travel_log.append(angle)
+            print("Path Stable - Continuing.")
+            end_mission = input("End mission? (y/n): ")
+            if end_mission == "y":
+                print("Mission Ended. Travel Log: " + str(travel_log))
+                break
+        
+    
+
